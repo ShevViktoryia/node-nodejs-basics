fed583b42d9a64a02998411cce11b03c1704b7f7
Victoria <shevvictoryia@gmail.com>
2024-10-06 23:36:31 +0300

    feat: add fs:rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,30 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "files", "wrongFilename.txt");
+const newFilePath = path.join(__dirname, "files", "properFilename.md");
+
 const rename = async () => {
-    // Write your code here 
+  // Write your code here
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      console.log("wrong file not found");
+      throw new Error("FS operation failed");
+    }
+    fs.access(newFilePath, fs.constants.F_OK, (err) => {
+      if (!err) {
+        console.log("properFilename.md already exists");
+        throw new Error("FS operation failed");
+      }
+      fs.rename(filePath, newFilePath, (err) => {
+        if (err) {
+          console.log("Rename Error");
+        }
+      });
+    });
+  });
 };
 
 await rename();
