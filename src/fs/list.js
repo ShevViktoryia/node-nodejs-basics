e7f5098402b14d5a4675032eabe482553cc458e8
Victoria <shevvictoryia@gmail.com>
2024-10-06 23:50:58 +0300

    feat: add fs:list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,26 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "files");
+
 const list = async () => {
-    // Write your code here 
+  // Write your code here
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      console.log("files folder not found");
+      throw new Error("FS operation failed");
+    }
+    fs.readdir(filePath, (err, files) => {
+      if (err) {
+        console.log("files read fall");
+      }
+      files.forEach((file) => {
+        console.log(file);
+      });
+    });
+  });
 };
 
 await list();
