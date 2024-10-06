d6fa110bfded19a3325ed83ff295fd68aa869967
Victoria <shevvictoryia@gmail.com>
2024-10-07 00:47:01 +0300

    feat: add cli:args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  const args = process.argv.slice(2);
+  const obj = {};
+  for (let i = 0; i < args.length; i += 2) {
+    const key = args[i].replace(/^--/, "");
+    const value = args[i + 1];
+    obj[key] = value;
+  }
+  const res = Object.entries(obj)
+    .map(([key, value]) => `${key} is ${value}`)
+    .join(", ");
+  console.log(res);
 };
 
 parseArgs();
