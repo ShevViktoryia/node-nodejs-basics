b33f7f2fe1872e93a02e3857f5cb659874bdb392
Victoria <shevvictoryia@gmail.com>
2024-10-07 00:33:30 +0300

    feat: add cli:env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  const res = Object.entries(process.env)
+    .map(([key, value]) => `RSS_${key}=${value}`)
+    .join("; ");
+  console.log(res);
 };
 
 parseEnv();
