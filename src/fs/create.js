0057e28ec89291b5c70f1473f0ce721bc41e4930
Victoria <shevvictoryia@gmail.com>
2024-10-06 17:15:32 +0300

    feat: add fs:create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const create = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, "files", "fresh.txt");
+  // Write your code here
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (!err) {
+      console.log("FS operation failed");
+      return new Error("FS operation failed");
+    }
+    fs.writeFile(filePath, "I am fresh and young", (err) => {
+      if (err) console.log("err");
+      else console.log("File was written");
+    });
+  });
 };
 
 await create();
