4d490dc37170942490ded354389b1ceceb7d75cb
Victoria <shevvictoryia@gmail.com>
2024-10-06 23:43:06 +0300

    feat: add fs:delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "files", "fileToRemove.txt");
+
 const remove = async () => {
-    // Write your code here 
+  // Write your code here
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      console.log("file to remove not found");
+      throw new Error("FS operation failed");
+    }
+  });
 };
 
 await remove();
