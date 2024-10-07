cc678a64cb8a9e7a11da78512df1308d09fc49dc
Victoria <shevvictoryia@gmail.com>
2024-10-08 00:51:34 +0300

    feat: add wt main

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,27 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { Worker } from "worker_threads";
+import os from "os";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "worker.js");
+const promises = [];
+
 const performCalculations = async () => {
-    // Write your code here
+  // Write your code here
+  for (let i = 0; i < os.cpus().length; i++) {
+    const num = 10 + i;
+    const worker = new Worker(filePath, { workerData: num });
+    const data = new Promise((resolve) => {
+      worker.on("message", (data) => resolve({ status: "resolved", data }));
+      worker.on("error", () => resolve({ status: "error", data: null }));
+    });
+    promises.push(data);
+  }
+  Promise.all(promises)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
 };
 
 await performCalculations();
