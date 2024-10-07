76cc0a5bc1cb54ffd2a4d571e95d36bb1dfd1f98
Victoria <shevvictoryia@gmail.com>
2024-10-08 00:15:14 +0300

    feat: add zip:compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import zlib from "zlib";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "files", "fileToCompress.txt");
+const outputPath = path.join(__dirname, "files", "archive.gz");
+const stream = fs.createReadStream(filePath);
+const output = fs.createWriteStream(outputPath);
+const gzip = zlib.createGzip();
+
 const compress = async () => {
-    // Write your code here 
+  // Write your code here
+  stream
+    .pipe(gzip)
+    .pipe(output)
+    .on("error", () => {
+      console.log("error");
+    });
 };
 
 await compress();
