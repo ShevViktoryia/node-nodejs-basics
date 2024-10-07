8301fc8d00f72d905a1148c387ceb4000bca1b76
Victoria <shevvictoryia@gmail.com>
2024-10-07 23:26:40 +0300

    feat: add streams:write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "files", "fileToWrite.txt");
+const output = fs.createWriteStream(filePath, { flags: "a" });
+
 const write = async () => {
-    // Write your code here 
+  // Write your code here
+  process.stdin.pipe(output);
+  output.on("error", () => {
+    console.log("error");
+  });
 };
 
 await write();
