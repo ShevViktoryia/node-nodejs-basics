78ec8a897295b572744e25ffa893801187bb089f
Victoria <shevvictoryia@gmail.com>
2024-10-07 22:55:32 +0300

    feat: add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import fs from "fs";
+import path from "path";
+import crypto from "crypto";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+const stream = fs.createReadStream(filePath);
+const hash = crypto.createHash("sha256");
+
 const calculateHash = async () => {
-    // Write your code here 
+  // Write your code here
+  stream.on("data", (data) => {
+    hash.update(data);
+  });
+  stream.on("end", () => {
+    console.log(hash.digest("hex"));
+  });
+  stream.on("error", () => {
+    console.log("error");
+  });
 };
 
 await calculateHash();
