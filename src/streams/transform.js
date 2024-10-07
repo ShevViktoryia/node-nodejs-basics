ddb459149cf78cc2cdc2fffb81d191ffa61fc798
Victoria <shevvictoryia@gmail.com>
2024-10-07 23:43:38 +0300

    feat: add streams:transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import stream from "stream";
+
 const transform = async () => {
   // Write your code here
+  const reverseStream = new stream.Transform({
+    transform(chunk, encoding, callback) {
+      const reverseStr = chunk.toString().split("").reverse().join("");
+      callback(null, reverseStr);
+    },
+  });
+  process.stdin.pipe(reverseStream).pipe(process.stdout);
 };
 
 await transform();
