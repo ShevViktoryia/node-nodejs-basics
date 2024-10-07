3372e0b826a5af8a50be64201d29eea44da0324f
Victoria <shevvictoryia@gmail.com>
2024-10-08 00:10:25 +0300

    fix: update streams:read

--- src/streams/read.js
+++ src/streams/read.js
@@ -5,23 +5,18 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const filePath = path.join(__dirname, "files", "fileToRead.txt");
-const outputPath = path.join(__dirname, "files", "process.stdout");
-const stream = fs.createReadStream(filePath);
-const output = fs.createWriteStream(outputPath);
+const stream = fs.createReadStream(filePath, { encoding: "utf8" });
 
 const read = async () => {
   // Write your code here
   stream.on("data", (data) => {
-    output.write(data);
+    process.stdout.write(data);
   });
   stream.on("end", () => {
-    output.end();
+    console.log("\n reading completed.");
   });
   stream.on("error", () => {
     console.log("read error");
-  });
-  output.on("error", () => {
-    console.log("write error");
   });
 };
 
